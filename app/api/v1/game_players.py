513cdd6fe764f40218ad3762ebf0a52cc3b73d42
Kilian321 <kvanovertvelt@gmail.com>
2026-05-18 01:02:02 +0200

    feat:game_player services

---
+++ app/api/v1/game_players.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from starlette import status
+
+from app.db.session import get_db
+from app.schemas.game_players import GamePlayersCreate, GamePlayersResponse, GamePlayersUpdate
+from app.services import game_player_service
+
+router = APIRouter(prefix="/game-players", tags=["Game Players"])
+
+
+@router.post("", response_model=GamePlayersResponse, status_code=status.HTTP_201_CREATED)
+def create_game_player(game_player: GamePlayersCreate, db: Session = Depends(get_db)):
+    return game_player_service.create_game_player(db, game_player)
+
+
+@router.get("/game/{game_id}", response_model=list[GamePlayersResponse])
+def get_players_by_game(game_id: int, db: Session = Depends(get_db)):
+    return game_player_service.get_players_by_game(db, game_id)
+
+
+@router.get("/{game_player_id}", response_model=GamePlayersResponse)
+def get_game_player_by_id(game_player_id: int, db: Session = Depends(get_db)):
+    return game_player_service.get_game_player(db, game_player_id)
+
+
+@router.delete("/{game_player_id}")
+def delete_game_player(game_player_id: int, db: Session = Depends(get_db)):
+    return game_player_service.delete_game_player(db, game_player_id)
+
+
+@router.patch("/{game_player_id}", response_model=GamePlayersResponse)
+def update_game_player(game_player_id: int, game_player_update: GamePlayersUpdate, db: Session = Depends(get_db), ):
+    return game_player_service.update_game_player(db, game_player_id, game_player_update)
--- app/models/game_players.py
+++ app/models/game_players.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, ForeignKey
+from sqlalchemy import Integer, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base import Base
@@ -9,12 +9,13 @@ class GamePlayers(Base):
     __tablename__ = 'game_players'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id'), nullable=False)
-    game_id: Mapped[int] = mapped_column(Integer, ForeignKey('games.id'), nullable=True)
-    roblox_id: Mapped[int] = mapped_column(Integer, nullable=True)
+    user_id: Mapped[int | None] = mapped_column(Integer, ForeignKey('users.id'), nullable=True)
+    game_id: Mapped[int] = mapped_column(Integer, ForeignKey('games.id'), nullable=False)
+    roblox_id: Mapped[int | None] = mapped_column(Integer, nullable=True)
+    pseudo: Mapped[str] = mapped_column(String(50), nullable=False)
     team: Mapped[Team] = mapped_column(TeamType)
-    kill: Mapped[int] = mapped_column(Integer, default=0)
-    death: Mapped[int] = mapped_column(Integer, default=0)
+    kills: Mapped[int] = mapped_column(Integer, default=0)
+    deaths: Mapped[int] = mapped_column(Integer, default=0)
 
     game = relationship("Game", back_populates="players")
     user = relationship("User", back_populates="game_players")
